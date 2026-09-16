e573a8d4003bae06292894181113f1554aaffff0
Natnael Tsegaye <ntsegaye@mdot.state.md.us>
2026-09-16 11:19:37 -0400

    adding pace tracker

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import storage
 import heart_rate
 import time
+from itertools import islice
 
 def main():
     user_age = user_age_prompt()
-    storage.set_initial_data(user_age)
+    username = user_name_prompt()
+    storage.set_initial_data(username)
     max_hr = heart_rate.calculate_current_max_hr(user_age)
-    zone_range = heart_rate.calculate_zones(max_hr)
+    target_zone = target_zone_prompt()
+    
+    start = time.time()
     while(running):
         current_hr = get_current_hr()
         curernt_zone = heart_rate.get_zone_status(current_hr, max_hr)
@@ -20,24 +24,62 @@ def main():
         time.sleep(1)
         while pause_run:
             time.sleep(0.5)
+
+        #user changes zone 
+        if(zone_change):
+            zones_data.Append({"zone": target_zone, "untilminute" : timer_from_watch})
+            target_zone = new_zone
+
+
+        #save pace every minute 
+        if(timer_every_minute)
+            minute_pace.Append(current_pace)
+
+        #save pace info every five minutes #minute_pace.clear() if you want to clear or do or minute_pace[::5]
+        if(timer_every_five):
+            average_pace = sum(minute_pace[::5]) /5 
+            pace_info.Append({"minute": timer_every_five, "zone": target_zone, "avg_pace": average_pace})
+
         if stop_run:
+            zones_data.Append({"zone": target_zone, "untilminute" : timer_from_watch})
             running = False
 
+    save_pace_entry(username, run_date, zones_data, pace_info):
+    storage.save_run_peak(username, user_age, heart_peak, run_date)
+    
 
 
 
 
+def target_zone_prompt():
+    zone_not_given = True
+    acceptable_Zone = [1,2,3,4,5]
+    while zone_not_given:
+        target_zone = input ("Heart Rate Zone (1-5): ")
+        int_zone = try_parse_int(target_zone)
+        if(int_zone is None or int_zone not in acceptable_zone):
+            print("Invalid zone, Try again")
 
 def user_age_prompt():
     age_not_given = True
     while age_not_given:
         user_age = input("Age: ")
         int_age = try_parse_int(user_age)
-        if int_age > 80 or int_age < 8:
+        if(int_age is None or int_age > 80 or int_age < 8):
             print("Invalid Age, Try again")
         else:
             age_not_given = False
     return int_age
+
+def user_name_prompt():
+        valid_name_not_given = True
+    while valid_name_not_given:
+        user_name = input("username (must be less than 5 letters): ")
+        if len(user_name) > 5:
+            print("Invalid name, Try again")
+        else:
+            valid_name_not_given = False
+    return user_name
 
 def try_parse_int(s, val=None):
   try:
--- storage.py
+++ storage.py
@@ -2,29 +2,29 @@ import json
 import os 
 from itertools import islice
 
-file_name = "user_data.json"
 
 
-def set_initial_data(age):
-    
+def set_initial_data(username):
+    file_name = get_file_name(username)
     if not os.path.exists(file_name):
         with open(file_name, "w") as f:
-            data = {"age": age, "run_history" : []}
+            data = {"run_history" : [], "pace_history" :[]}
             json.dump(data, f, indent=4)
         
 
-def save_run_peak(heart_rate_peak, run_date):
+def save_run_peak(username, age, heart_rate_peak, run_date):
+
+    file_name = get_file_name(username)
     with open(file_name, "r") as f:
-        data = json.load(f)
-        
-    data["run_history"].append({"date": run_date, "peak_hr": heart_rate_peak})
+        data = json.load(f)  
+    data["run_history"].append({"date": run_date, "peak_hr": heart_rate_peak, "age": age})
 
     with open(file_name, "w") as f:
         json.dump(data, f, indent=4)
 
-def calculate_current_max_hr(age):
+def calculate_current_max_hr(username, age):
     current_max_hr = 208 - (0.7 * age)
-
+    file_name = get_file_name(username)
     if os.path.exists(file_name):
         with open(file_name, "r") as f:
             data = json.load(f)
@@ -37,9 +37,52 @@ def calculate_current_max_hr(age):
 
     return current_max_hr
 
+
+def get_file_name(username):
+    return "user_" + str(username) +".json"
 if __name__ == "__main__":
     set_initial_data(25)
     save_run_peak(188, "2026-09-10")
     save_run_peak(192, "2026-09-12")
     save_run_peak(178, "2026-09-14")
     print(calculate_current_max_hr(25))
+
+
+
+def save_pace_entry(username, run_date, target_zones, blocks):
+    file_name = get_file_name(username)
+    with open(file_name, "r") as f:
+        data = json.load(f)  
+    data["pace_history"].Append({"date": run_date, "target_zones": target_zones, "blocks" : blocks})
+
+    with open(file_name, "w") as f:
+        json.dump(data, f, indent=4)
+
+
+
+
+
+
+#"run_history" : [
+#   {
+#       "date" : "2026-09-14",
+#       "peak_hr" : "190",
+#       "age" : 25
+#   }
+# ]
+#"pace_history": [
+#    {
+#        "date": "2026-09-14",
+#        "target_zones": [
+#            {"zone": 2, "until_minute": 20},
+#            {"zone": 3, "until_minute": 40}
+#        ],
+#        "blocks": [
+#            {"minute": 5, "zone": 2, "avg_pace": "9:30"},
+#            {"minute": 10, "zone": 2, "avg_pace": "9:45"},
+#            {"minute": 15, "zone": 3, "avg_pace": "8:20"},
+#            {"minute": 20, "zone": 2, "avg_pace": "9:50"},
+#            {"minute": 25, "zone": 3, "avg_pace": "8:40"}
+#        ]
+#    }
+#]
